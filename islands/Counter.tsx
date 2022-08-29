b4bcde3fdf9fd108cff5d3b122ab700cc3006acd
thanh.nguyen21 <thanh.nguyen21@mservice.com.vn>
2022-08-30 01:11:52 +0700

    init the repo

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -13,7 +13,7 @@ export default function Counter(props: CounterProps) {
   useEffect(() => {
     const canvas = document.getElementById("renderCanvas")
     console.log(canvas)
-    // const engine = new BABYLON.default.Engine(canvas, true)
+    const engine = new BABYLON.default.Engine(canvas, true)
     // const scene = createScene(canvas, engine)
     // engine.runRenderLoop(() => {
     //   scene.render()
