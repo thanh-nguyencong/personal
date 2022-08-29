6ae8e76c3174829c025cd4813cb04c2e13febc62
thanh.nguyen21 <thanh.nguyen21@mservice.com.vn>
2022-08-30 01:38:05 +0700

    init the repo

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -13,11 +13,11 @@ export default function Counter(props: CounterProps) {
   useEffect(() => {
     function createScene(canvas: HTMLElement | null, engine: BABYLON.default.Engine) {
       const scene = new BABYLON.default.Scene(engine)
-      BABYLON.default.SceneLoader.ImportMeshAsync("", "https://assets.babylonjs.com/meshes/", "box.babylon")
-      const camera = new BABYLON.default.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.default.Vector3(0, 0, 0))
+      // BABYLON.default.SceneLoader.ImportMeshAsync("", "https://assets.babylonjs.com/meshes/", "box.babylon")
+      // const camera = new BABYLON.default.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.default.Vector3(0, 0, 0))
       // camera.attachControl(canvas, true)
-      const light = new BABYLON.default.HemisphericLight("light", new BABYLON.default.Vector3(1, 1, 0))
-      return scene
+      // const light = new BABYLON.default.HemisphericLight("light", new BABYLON.default.Vector3(1, 1, 0))
+      // return scene
     }
 
     const canvas = document.getElementById("renderCanvas")
