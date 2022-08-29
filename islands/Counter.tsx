cfe9de512bf3426223eda3cb27489eb129425d0c
thanh.nguyen21 <thanh.nguyen21@mservice.com.vn>
2022-08-29 19:42:03 +0700

    init the repo

---
+++ islands/Counter.tsx
@@ -0,0 +1,34 @@
+/** @jsx h */
+import { h } from "preact";
+import { useEffect, useState } from "preact/hooks";
+import { tw } from "@twind";
+
+import * as BABYLON from "https://esm.sh/babylonjs@5.21.0";
+
+interface CounterProps {
+  start: number;
+}
+
+export default function Counter(props: CounterProps) {
+  useEffect(() => {
+    const canvas = Object.create(document.getElementById("renderCanvas")!)
+    const engine = new BABYLON.default.Engine(canvas, true)
+    const scene = createScene(canvas, engine)
+    engine.runRenderLoop(() => {
+      scene.render()
+    })
+
+  }, [])
+  return (
+    <canvas id ="renderCanvas"></canvas>
+  );
+}
+
+function createScene(canvas: HTMLElement, engine: BABYLON.default.Engine) {
+  const scene = new BABYLON.default.Scene(engine)
+  BABYLON.default.SceneLoader.ImportMeshAsync("", "https://assets.babylonjs.com/meshes/", "box.babylon")
+  const camera = new BABYLON.default.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.default.Vector3(0, 0, 0))
+  camera.attachControl(canvas, true)
+  const light = new BABYLON.default.HemisphericLight("light", new BABYLON.default.Vector3(1, 1, 0))
+  return scene
+}
