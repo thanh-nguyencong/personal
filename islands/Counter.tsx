c61a32ae7b7ca17b7055eb44985c1198647c068a
thanh.nguyen21 <thanh.nguyen21@mservice.com.vn>
2022-08-30 13:20:39 +0700

    init the repo

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -2,32 +2,30 @@
 import { h } from "preact";
 import { useEffect, useRef, useState } from "preact/hooks";
 import { tw } from "@twind";
-import * as b from 'https://esm.sh/babylonjs@5.21.0';
-
+import * as BABYLON from 'https://esm.sh/babylonjs@5.21.0';
+console.log(BABYLON)
 
 export default function Counter() {
-  const reactCanvas = useRef(null);
-  useEffect(() => {
-    const { current: canvas } = reactCanvas;
-    if (!canvas) return
-    console.log(navigator.hardwareConcurrency)
-    console.log(b)
-    function createScene(canvas: HTMLElement | null) {
-      // const scene = new BABYLON.default.Scene(engine)
-      // BABYLON.default.SceneLoader.ImportMeshAsync("", "https://assets.babylonjs.com/meshes/", "box.babylon")
-      // const camera = new BABYLON.default.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.default.Vector3(0, 0, 0))
-      // camera.attachControl(canvas, true)
-      // const light = new BABYLON.default.HemisphericLight("light", new BABYLON.default.Vector3(1, 1, 0))
-      // return scene
-    }
+  // const reactCanvas = useRef(null);
+  // useEffect(() => {
+  //   const { current: canvas } = reactCanvas;
+  //   if (!canvas) return
+  //   function createScene(canvas: HTMLElement | null, engine: BABYLON.default.Engine) {
+  //     const scene = new BABYLON.default.Scene(engine)
+  //     BABYLON.default.SceneLoader.ImportMeshAsync("", "https://assets.babylonjs.com/meshes/", "box.babylon")
+  //     const camera = new BABYLON.default.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.default.Vector3(0, 0, 0))
+  //     camera.attachControl(canvas, true)
+  //     const light = new BABYLON.default.HemisphericLight("light", new BABYLON.default.Vector3(1, 1, 0))
+  //     return scene
+  //   }
 
-    // const engine = new BABYLON.default.Engine(canvas, true)
-    // const scene = createScene(canvas, engine)
-    // engine.runRenderLoop(() => {
-    //   scene.render()
-    // })
+  //   const engine = new BABYLON.default.Engine(canvas, true)
+  //   const scene = createScene(canvas, engine)
+  //   engine.runRenderLoop(() => {
+  //     scene.render()
+  //   })
 
-  })
+  // })
   return (
     <div>
       <canvas ref={reactCanvas}></canvas>
